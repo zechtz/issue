d876d54301bcf87b8966fb0ea341ae4619d11b86
Zachariah Ngonyani <zachariahn@gmail.com>
2020-04-14 22:07:57 +0300

    Finish up creating a script

--- README.md
+++ README.md
@@ -1,21 +1,13 @@
 # Issues
 
-**TODO: Add description**
+**Fetches latest issues from github and prints in tabular form**
 
-## Installation
+## Running the script
 
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `issues` to your list of dependencies in `mix.exs`:
+Make sure you have erlang installed in your computer then just run 
 
-```elixir
-def deps do
-  [
-    {:issues, "~> 0.1.0"}
-  ]
-end
-```
+```./issues <githubuser> <githubprojectname> <numberofissues>```
 
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at [https://hexdocs.pm/issues](https://hexdocs.pm/issues).
+## Testing
 
+```mix test```
--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -1,78 +1,80 @@
 defmodule Issues.Cli do
-  alias Issues.GithubIssues
-  import Issues.TableFormatter , only: [print_table_for_columns: 2]
+
+  import Issues.TableFormatter, only: [ print_table_for_columns: 2 ]
 
   @default_count 4
 
   @moduledoc """
-  Handle the commandline parsing and the dispatch to
+  Handle the command line parsing and the dispatch to
   the various functions that end up generating a
-  table f the last n_issues in a github project
+  table of the last _n_ issues in a github project
   """
 
-  def run(argv) do
+  def main(argv) do
     argv
     |> parse_args
     |> process
   end
 
   @doc """
-  `argv` can be -h or --help which returns :help.
+  `argv` can be -h or --help, which returns   `:help`.
 
-  Otherwise it is a github username, project name, and (optionally)
+  Otherwise it is a github user name, project name, and (optionally)
   the number of entries to format
 
-  Return a tuple of `{user, project, count}` or `:help` if help was given
+  Return a tuple of `{ user, project, count }`, or `nil` if help was given.
   """
   def parse_args(argv) do
-    OptionParser.parse(argv, switches: [help: :boolean], aliases:  [h: :help])
-    |> elem(1)
-    |> args_to_internal_representation()
+    OptionParser.parse(argv, switches: [ help: :boolean],
+      aliases:  [ h:    :help   ])
+      |> elem(1)
+      |> args_to_internal_representation()
+  end
+
+  def args_to_internal_representation([user, project, count]) do
+    { user, project, String.to_integer(count) }
+  end
+
+  def args_to_internal_representation([user, project]) do
+    { user, project, @default_count }
+  end
+
+  def args_to_internal_representation(_) do # bad arg or --help
+    :help
   end
 
   def process(:help) do
     IO.puts """
-    usage: issues <user> <project> [ count | #{@default_count} ]
+    usage:  issues <user> <project> [ count | #{@default_count} ]
     """
     System.halt(0)
   end
 
-  def process({user, project,count}) do
-    GithubIssues.fetch(user, project)
-    |> decode_response
-    |> sort_into_descending_order
+  def process({user, project, count}) do
+    Issues.GithubIssues.fetch(user, project)
+    |> decode_response()
+    |> sort_into_descending_order()
     |> last(count)
     |> print_table_for_columns(["number", "created_at", "title"])
   end
 
-  def sort_into_descending_order(issues) do
-    issues
-    |> Enum.sort(fn(i1, i2) ->
-      i1["created_at"] >= i2["created_at"]
-    end)
+  def decode_response({:ok, body}), do: body
+
+  def decode_response({:error, error}) do
+    IO.puts "Error fetching from Github: #{error["message"]}"
+    System.halt(2)
   end
 
-  defp decode_response({:ok, body}), do: body
-
-  defp decode_response({:error, error}) do
-    IO.puts "Error fetching Github: #{error["message"]}"
-  end
-
-  defp args_to_internal_representation([user, project, count]) do
-    { user, project, String.to_integer(count) }
-  end
-
-  defp args_to_internal_representation([user, project]) do
-    { user, project, @default_count }
-  end
-
-  defp args_to_internal_representation(_) do
-    :help
-  end
-
-  defp last(list, count) do
+  def last(list, count) do
     list
     |> Enum.take(count)
     |> Enum.reverse
+  end
+
+  def sort_into_descending_order(list_of_issues) do
+    list_of_issues
+    |> Enum.sort(fn i1, i2 ->
+      i1["created_at"] >= i2["created_at"]
+    end)
   end
 end
